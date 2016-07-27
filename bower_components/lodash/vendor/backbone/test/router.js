e6a32075838c6105f637a1dbe217c2fb08c8c986
JoffreyHernandez <skool31>
2016-07-27 15:41:43 +0200

    UI ok manque le scroll spy

--- bower_components/lodash/vendor/backbone/test/router.js
+++ bower_components/lodash/vendor/backbone/test/router.js
@@ -1,4 +1,4 @@
-(function() {
+(function (QUnit) {
 
   var router = null;
   var location = null;
@@ -29,6 +29,7 @@
         'pathname',
         'protocol'
       ));
+
       // In IE, anchor.pathname does not contain a leading slash though
       // window.location.pathname does.
       if (!/^\//.test(this.pathname)) this.pathname = '/' + this.pathname;
@@ -42,7 +43,7 @@
 
   QUnit.module('Backbone.Router', {
 
-    setup: function() {
+    beforeEach: function () {
       location = new Location('http://example.com');
       Backbone.history = _.extend(new Backbone.History, {location: location});
       router = new Router({testing: 101});
@@ -53,7 +54,7 @@
       Backbone.history.on('route', onRoute);
     },
 
-    teardown: function() {
+    afterEach: function () {
       Backbone.history.stop();
       Backbone.history.off('route', onRoute);
     }
@@ -95,6 +96,10 @@
       '*anything': 'anything'
     },
 
+    preinitialize: function (options) {
+      this.testpreinit = 'foo';
+    },
+
     initialize: function(options) {
       this.testing = options.testing;
       this.route('implicit', 'implicit');
@@ -121,19 +126,19 @@
       this.charType = 'escaped';
     },
 
-    contacts: function(){
+    contacts: function () {
       this.contact = 'index';
     },
 
-    newContact: function(){
+    newContact: function () {
       this.contact = 'new';
     },
 
-    loadContact: function(){
+    loadContact: function () {
       this.contact = 'load';
     },
 
-    optionalItem: function(arg){
+    optionalItem: function (arg) {
       this.arg = arg !== void 0 ? arg : null;
     },
 
@@ -179,6 +184,11 @@
   QUnit.test('initialize', function(assert) {
     assert.expect(1);
     assert.equal(router.testing, 101);
+  });
+
+  QUnit.test('preinitialize', function (assert) {
+    assert.expect(1);
+    assert.equal(router.testpreinit, 'foo');
   });
 
   QUnit.test('routes (simple)', function(assert) {
@@ -234,10 +244,11 @@
     assert.ok(Backbone.history.navigate('search/manhattan/p20', true));
   });
 
-  QUnit.test('route precedence via navigate', function(assert){
+  QUnit.test('route precedence via navigate', function (assert) {
     assert.expect(6);
-    // check both 0.9.x and backwards-compatibility options
-    _.each([{trigger: true}, true], function( options ){
+
+    // Check both 0.9.x and backwards-compatibility options
+    _.each([{trigger: true}, true], function (options) {
       Backbone.history.navigate('contacts', options);
       assert.equal(router.contact, 'index');
       Backbone.history.navigate('contacts/new', options);
@@ -249,7 +260,9 @@
 
   QUnit.test('loadUrl is not called for identical routes.', function(assert) {
     assert.expect(0);
-    Backbone.history.loadUrl = function(){ assert.ok(false); };
+    Backbone.history.loadUrl = function () {
+      assert.ok(false);
+    };
     location.replace('http://example.com#route');
     Backbone.history.navigate('route');
     Backbone.history.navigate('/route');
@@ -345,9 +358,11 @@
     assert.strictEqual(router.path, 'c/d/e');
   });
 
-  QUnit.test("fires event when router doesn't have callback on it", function(assert) {
+  QUnit.test('fires event when router doesn\'t have callback on it', function (assert) {
     assert.expect(1);
-    router.on('route:noCallback', function(){ assert.ok(true); });
+    router.on('route:noCallback', function () {
+      assert.ok(true);
+    });
     location.replace('http://example.com#noCallback');
     Backbone.history.checkUrl();
   });
@@ -536,8 +551,10 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(){},
-        replaceState: function(){}
+        pushState: function () {
+        },
+        replaceState: function () {
+        }
       }
     });
     Backbone.history.start({root: 'root'});
@@ -551,8 +568,9 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(){},
-        replaceState: function(state, title, url){
+        pushState: function () {
+        },
+        replaceState: function (state, title, url) {
           assert.strictEqual(url, '/root/x/y');
         }
       }
@@ -570,8 +588,10 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(){},
-        replaceState: function(){}
+        pushState: function () {
+        },
+        replaceState: function () {
+        }
       }
     });
     Backbone.history.start({root: ''});
@@ -625,8 +645,9 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(){},
-        replaceState: function(state, title, url){
+        pushState: function () {
+        },
+        replaceState: function (state, title, url) {
           assert.strictEqual(url, '/root/x/y?a=b');
         }
       }
@@ -641,8 +662,9 @@
     assert.expect(1);
     var MyRouter = Backbone.Router.extend({
       routes: {'': 'empty'},
-      empty: function(){},
-      route: function(route){
+      empty: function () {
+      },
+      route: function (route) {
         assert.strictEqual(route, '');
       }
     });
@@ -655,7 +677,8 @@
     assert.strictEqual(history.getFragment('fragment   '), 'fragment');
   });
 
-  QUnit.test('#1820 - Leading slash and trailing space.', 1, function(assert) {
+  QUnit.test('#1820 - Leading slash and trailing space.', function (assert) {
+    assert.expect(1);
     var history = new Backbone.History;
     assert.strictEqual(history.getFragment('/fragment '), 'fragment');
   });
@@ -670,7 +693,7 @@
     assert.strictEqual(router.z, '123');
   });
 
-  QUnit.test("#2062 - Trigger 'route' event on router instance.", function(assert) {
+  QUnit.test('#2062 - Trigger "route" event on router instance.', function (assert) {
     assert.expect(2);
     router.on('route', function(name, args) {
       assert.strictEqual(name, 'routeEvent');
@@ -709,8 +732,11 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(){},
-        replaceState: function(){ assert.ok(false); }
+        pushState: function () {
+        },
+        replaceState: function () {
+          assert.ok(false);
+        }
       }
     });
     Backbone.history.start({
@@ -726,8 +752,10 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(){},
-        replaceState: function(){}
+        pushState: function () {
+        },
+        replaceState: function () {
+        }
       }
     });
 
@@ -753,7 +781,7 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(state, title, url){
+        pushState: function (state, title, url) {
           assert.strictEqual(url, '/root');
         }
       }
@@ -785,7 +813,7 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(state, title, url){
+        pushState: function (state, title, url) {
           assert.strictEqual(url, '/root?x=1');
         }
       }
@@ -823,7 +851,7 @@
     assert.expect(1);
     var MyRouter = Backbone.Router.extend({
       routes: {
-        path: function(params){
+        path: function (params) {
           assert.strictEqual(params, 'x=y%3Fz');
         }
       }
@@ -921,7 +949,8 @@
     Backbone.history = _.extend(new Backbone.History, {location: location});
     var MyRouter = Backbone.Router.extend({
       routes: {'foo/:id/bar': 'foo'},
-      foo: function(){},
+      foo: function () {
+      },
       execute: function(callback, args, name) {
         assert.strictEqual(callback, this.foo);
         assert.deepEqual(args, ['123', 'x=y']);
@@ -953,8 +982,12 @@
     Backbone.history = _.extend(new Backbone.History, {
       location: location,
       history: {
-        pushState: function(){ assert.ok(false); },
-        replaceState: function(){ assert.ok(false); }
+        pushState: function () {
+          assert.ok(false);
+        },
+        replaceState: function () {
+          assert.ok(false);
+        }
       }
     });
     Backbone.history.start({pushState: true});
@@ -991,14 +1024,14 @@
     Backbone.history.start({root: '/root', pushState: true});
   });
 
-  QUnit.test("Paths that don't match the root should not match no root", function(assert) {
+  QUnit.test('Paths that don\'t match the root should not match no root', function (assert) {
     assert.expect(0);
     location.replace('http://example.com/foo');
     Backbone.history.stop();
     Backbone.history = _.extend(new Backbone.History, {location: location});
     var MyRouter = Backbone.Router.extend({
       routes: {
-        foo: function(){
+        foo: function () {
           assert.ok(false, 'should not match unless root matches');
         }
       }
@@ -1007,14 +1040,14 @@
     Backbone.history.start({root: 'root', pushState: true});
   });
 
-  QUnit.test("Paths that don't match the root should not match roots of the same length", function(assert) {
+  QUnit.test('Paths that don\'t match the root should not match roots of the same length', function (assert) {
     assert.expect(0);
     location.replace('http://example.com/xxxx/foo');
     Backbone.history.stop();
     Backbone.history = _.extend(new Backbone.History, {location: location});
     var MyRouter = Backbone.Router.extend({
       routes: {
-        foo: function(){
+        foo: function () {
           assert.ok(false, 'should not match unless root matches');
         }
       }
@@ -1029,7 +1062,11 @@
     Backbone.history.stop();
     Backbone.history = _.extend(new Backbone.History, {location: location});
     var MyRouter = Backbone.Router.extend({
-      routes: {foo: function(){ assert.ok(true); }}
+      routes: {
+        foo: function () {
+          assert.ok(true);
+        }
+      }
     });
     var myRouter = new MyRouter;
     Backbone.history.start({root: 'x+y.z', pushState: true});
@@ -1041,7 +1078,11 @@
     Backbone.history.stop();
     Backbone.history = _.extend(new Backbone.History, {location: location});
     var MyRouter = Backbone.Router.extend({
-      routes: {foo: function(){ assert.ok(true); }}
+      routes: {
+        foo: function () {
+          assert.ok(true);
+        }
+      }
     });
     var myRouter = new MyRouter;
     Backbone.history.start({root: '®ooτ', pushState: true});
@@ -1053,10 +1094,21 @@
     Backbone.history.stop();
     Backbone.history = _.extend(new Backbone.History, {location: location});
     var MyRouter = Backbone.Router.extend({
-      routes: {'': function(){ assert.ok(true); }}
+      routes: {
+        '': function () {
+          assert.ok(true);
+        }
+      }
     });
     var myRouter = new MyRouter;
     Backbone.history.start({root: '®ooτ', pushState: true});
   });
 
-})();
+  QUnit.test('#4025 - navigate updates URL hash as is', function (assert) {
+    assert.expect(1);
+    var route = 'search/has%20space';
+    Backbone.history.navigate(route);
+    assert.strictEqual(location.hash, '#' + route);
+  });
+
+})(QUnit);
--- bower_components/lodash/vendor/backbone/test/view.js
+++ bower_components/lodash/vendor/backbone/test/view.js
@@ -1,10 +1,10 @@
-(function() {
+(function (QUnit) {
 
   var view;
 
   QUnit.module('Backbone.View', {
 
-    beforeEach: function(assert) {
+    beforeEach: function () {
       $('#qunit-fixture').append(
         '<div id="testElement"><h1>Test</h1></div>'
       );
@@ -61,6 +61,28 @@
     assert.strictEqual(new View().one, 1);
   });
 
+  QUnit.test('preinitialize', function (assert) {
+    assert.expect(1);
+    var View = Backbone.View.extend({
+      preinitialize: function () {
+        this.one = 1;
+      }
+    });
+
+    assert.strictEqual(new View().one, 1);
+  });
+
+  QUnit.test('preinitialize occurs before the view is set up', function (assert) {
+    assert.expect(2);
+    var View = Backbone.View.extend({
+      preinitialize: function () {
+        assert.equal(this.el, undefined);
+      }
+    });
+    var _view = new View({});
+    assert.notEqual(_view.el, undefined);
+  });
+
   QUnit.test('render', function(assert) {
     assert.expect(1);
     var myView = new Backbone.View;
@@ -72,8 +94,12 @@
     var counter1 = 0, counter2 = 0;
 
     var myView = new Backbone.View({el: '#testElement'});
-    myView.increment = function(){ counter1++; };
-    myView.$el.on('click', function(){ counter2++; });
+    myView.increment = function () {
+      counter1++;
+    };
+    myView.$el.on('click', function () {
+      counter2++;
+    });
 
     var events = {'click h1': 'increment'};
 
@@ -129,11 +155,10 @@
     assert.equal(myView.counter, 3);
   });
 
-
   QUnit.test('delegateEvents ignore undefined methods', function(assert) {
     assert.expect(0);
     var myView = new Backbone.View({el: '<p></p>'});
-    myView.delegateEvents({'click': 'undefinedMethod'});
+    myView.delegateEvents({click: 'undefinedMethod'});
     myView.$el.trigger('click');
   });
 
@@ -142,8 +167,12 @@
     var counter1 = 0, counter2 = 0;
 
     var myView = new Backbone.View({el: '#testElement'});
-    myView.increment = function(){ counter1++; };
-    myView.$el.on('click', function(){ counter2++; });
+    myView.increment = function () {
+      counter1++;
+    };
+    myView.$el.on('click', function () {
+      counter2++;
+    });
 
     var events = {'click h1': 'increment'};
 
@@ -203,7 +232,9 @@
     assert.expect(2);
     var myView = new Backbone.View({el: '#testElement'});
     myView.delegate('click', function() { assert.ok(true); });
-    var handler = function(){ assert.ok(false); };
+    var handler = function () {
+      assert.ok(false);
+    };
     myView.delegate('click', 'h1', handler);
     myView.undelegate('click', 'h1', handler);
     myView.$('h1').trigger('click');
@@ -405,8 +436,12 @@
     assert.expect(0);
     var View = Backbone.View.extend({
       initialize: function() {
-        this.listenTo(this.model, 'all x', function(){ assert.ok(false); });
-        this.listenTo(this.collection, 'all x', function(){ assert.ok(false); });
+        this.listenTo(this.model, 'all x', function () {
+          assert.ok(false);
+        });
+        this.listenTo(this.collection, 'all x', function () {
+          assert.ok(false);
+        });
       }
     });
 
@@ -492,4 +527,4 @@
     assert.notEqual($oldEl, myView.$el);
   });
 
-})();
+})(QUnit);
